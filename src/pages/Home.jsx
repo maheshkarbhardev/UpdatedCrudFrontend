c61edb281a31ff518e3488b517fcbe9bf84c124c
maheshkarbhardev <maheshkarbhar.dev@gmail.com>
2025-04-21 16:27:00 +0530

    Delete API Integrated.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Home = () => {
   const [employee, setEmployee] = useState([]);
@@ -7,6 +8,17 @@ const Home = () => {
   const fetchEmployee = async () => {
     const response = await axios.get("http://localhost:5000/api/get");
     setEmployee(response.data);
+  };
+
+  const deleteEmployee = (id) => {
+    if (window.confirm("Do You want to delete this employee?")) {
+      axios.delete(`http://localhost:5000/api/delete/${id}`);
+      toast.success("Employee Deleted Successfully");
+    }
+
+    setTimeout(() => {
+      fetchEmployee();
+    }, 300);
   };
 
   useEffect(() => {
@@ -26,48 +38,50 @@ const Home = () => {
 
       <div>
         <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Department</th>
-                    <th>Interests</th>
-                    <th>BirthDate</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>Department</th>
+              <th>Interests</th>
+              <th>BirthDate</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
 
-            <tbody>
-                {
-                    employee.map((item,index)=>{
-                        const formattedDate=new Date(item.birthDate).toISOString().split("T")[0];
-                        return(
-                            <tr key={item.id}>
-                                <td>{index+1}</td>
-                                <td>{item.firstName}</td>
-                                <td>{item.lastName}</td>
-                                <td>{item.email}</td>
-                                <td>{item.phoneNumber}</td>
-                                <td>{item.age}</td>
-                                <td>{item.gender}</td>
-                                <td>{item.department}</td>
-                                <td>{item.interests}</td>
-                                <td>{formattedDate}</td>
-                                <td>
-                                    <button>Edit</button>
-                                    <button>Delete</button>
-                                    <button>View</button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
+          <tbody>
+            {employee.map((item, index) => {
+              const formattedDate = new Date(item.birthDate)
+                .toISOString()
+                .split("T")[0];
+              return (
+                <tr key={item.id}>
+                  <td>{index + 1}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phoneNumber}</td>
+                  <td>{item.age}</td>
+                  <td>{item.gender}</td>
+                  <td>{item.department}</td>
+                  <td>{item.interests}</td>
+                  <td>{formattedDate}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button onClick={() => deleteEmployee(item.id)}>
+                      Delete
+                    </button>
+                    <button>View</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
